92a8da30ec69ce985b82b07250ea4e65e1509c84
Alain Saint-Dic <alains214@gmail.com>
2020-06-08 21:19:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,5 @@
 def unsafe?(speed)
- if speed < 40
-   return true
- elsif speed > 60 
+ if speed < 40 || speed > 60
    return true
   else 
     return false
@@ -12,9 +10,10 @@ end
 
 
 def not_safe?(speed)
-  speed < 40 ? true : false
-	speed > 60 ? true : false
-end
-	
 
+speed < 40 || speed > 60 ? true : false
+#have to put in >40 somewhere
+end
+
+	
 
